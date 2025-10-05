a801fa9b0c4e122a13a3712e612aecdf02d8d7cd
Tomodo1773 <forever-career@hotmail.co.jp>
2025-10-05 16:17:39 +0900

    refactor(tests): 修正されたインポートパスを更新

--- src/api/tests/jquants/test_jquants.py
+++ src/api/tests/jquants/test_jquants.py
@@ -5,7 +5,7 @@ import os
 import pytest
 from dotenv import load_dotenv
 
-from api.stock.services.jquants_service import JQuantsClient
+from stock.services.jquants_service import JQuantsClient
 
 
 @pytest.fixture
